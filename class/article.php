ca8e69c2b844b74e526374c84576c31d8c0b693f
thomas-dellacase <thomas.dellacase@laplatefrome.io>
2021-02-08 12:04:13 +0100

    anti injection sql

--- class/article.php
+++ class/article.php
@@ -11,9 +11,20 @@
         public function __construct(){
             $this->db = connect();
         }
+
+// ----------------------------- Anti injection sql --------------------------------------
+        function cleanQuerre($article){
+            if(get_magic_quotes_gpc())   // pas de duplicate backslashes
+                $article = stripslashes($article);
+            return mysql_escape_string($article);
+        }
+
 // ----------------------------- Créé article --------------------------------------
 
-        
+        public function create($article, $id_utilisateur, $id_categorie, $date){
+
+
+        }
     }
 
 
