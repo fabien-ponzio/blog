c3dc610ff5c9e2792fd6c299bd71e86e9d769313
thomas-dellacase <thomas.dellacase@laplatefrome.io>
2021-02-09 13:21:16 +0100

    correction

--- class/article.php
+++ class/article.php
@@ -1,20 +1,6 @@
 <?php
     require_once('../functions/db.php');
-
-    class article{
-        private $id;
-        public $article;
-        public $id_utilisateur;
-        public $id_categorie;
-        public $date;
-
-        public function __construct(){
-            $this->db = connect();
-        }
-// ----------------------------- Créé article --------------------------------------
-
-        
-    }
+    require_once('../class/article.php');
 
 
 ?>
