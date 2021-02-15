d1cc93ec124c8bbeaac41ff7822340ad35c33724
Fabien <fabien.ponzio@laplateforme.io>
2021-02-15 14:45:06 +0100

    Update admin.php

--- pages/admin.php
+++ pages/admin.php
@@ -58,3 +58,19 @@ require_once('../class/classAdmin.php');
     </form>
 </body>
 </html>
+// ------------------------------------------------ UPDATE ID DROITS -------------------------------------------------------------
+<?php
+
+public function updateRights($login, $id_droits){
+    
+    $query = $this->db->prepare ("UPDATE utilisateurs SET id_droits=:id WHERE login=:login"); 
+
+    $query->bindValue(":id", $id_droits, PDO::PARAM_INT); 
+    $query->bindValue(":login", $login, PDO::PARAM_STR); 
+
+    $query->execute();
+    var_dump($query); 
+
+
+}
+?>
