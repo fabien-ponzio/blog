a92498cc24a6794d5178d451f6a1811ec3e8162d
thomas-dellacase <thomas.dellacase@laplateforme.io>
2021-02-19 11:07:10 +0100

    rty

--- class/classCommentaire.php
+++ class/classCommentaire.php
@@ -38,6 +38,8 @@ class Commentaires
         echo $errorCom;
     }
 
-
-
+// ----------------------------- display commentaire -------------------------------------
+public function displayComment(){
+    
+}
 }
