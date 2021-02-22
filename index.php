3e6603a01dde5bb05c121362d39d627f00befcaf
DEJOUX Jeremy <jeremy.dejoux@laplateforme.io>
2021-02-22 17:35:09 +0100

    index article fon noir

--- index.php
+++ index.php
@@ -25,6 +25,7 @@ require_once("class/class-article.php");
     <link rel="stylesheet" href="css/header.css">
     <link rel="stylesheet" href="css/footer.css">
     <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/articles.css">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
