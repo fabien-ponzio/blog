a608833cc98b5d866415bee13f6609fa5682da1a
Jeremy <jeremy.dejoux@laplateforme.io>
2021-02-22 16:00:24 +0100

    corchet

--- pages/profil.php
+++ pages/profil.php
@@ -24,47 +24,49 @@ require_once('../class/user.php');
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="../css/header.css">
-    <link rel="stylesheet" href="../css/profil.css"
+    <link rel="stylesheet" href="../css/profil.css">
     <link rel="stylesheet" href="../css/footer.css">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>profil</title>
 </head>
 <body>
-<?php require_once('header.php');?>
-<main class="flex a_center column j_around" id="main_connexion">
-    <form action="profil.php" method="POST" id="formulaire_edition" class="flex a_center column j_around">
+    <?php require_once('header.php');?>
 
-        <section class="flex column a_center j_center">
-            <label for="newLogin">Nouveau pseudo</label>
-            <input class="form_input" type="text"  name="newlogin" placeholder="Login">
-        </section>
+    <main class="flex a_center column j_around" id="main_connexion">
 
-        <section class="flex column a_center">
-            <label for="newmail">Nouvelle adresse mail</label>
-            <input type="email" name="newMail">
-        </section>
+        <form action="profil.php" method="POST" id="formulaire_edition" class="flex a_center column j_around">
 
-        <section class="flex j_around a_center">
-            <article class="flex column j_center a_center">
-                <label for="oldPassword">New password</label>
-                <input class="form_input" type="password" name="newpassword" placeholder="New password">
-            </article>
-            <article class="flex column j_center a_center">
-                <label for="newPassword">Confirm password</label>
-                <input class="form_input" type="password" name="confpassword" placeholder="Confirm Password">
-            </article>
-        </section>
-        <button type="submit" name="submit" value="Envoyer">Mettre à jour mon profil</button>
+            <section class="flex column a_center j_center">
+                <label for="newLogin">Nouveau pseudo</label>
+                <input class="form_input" type="text"  name="newlogin" placeholder="Login">
+            </section>
 
-        <?php 
-            if (isset($_POST['submit'])){
-                $user = new User;
-                $user->profile($_POST['newlogin'],$_POST['newMail'],$_POST['newpassword'],$_POST['confpassword']);
-            }
-        ?> 
-    </form>
-</main>
-<?php require_once('footer.php');?>
+            <section class="flex column a_center">
+                <label for="newmail">Nouvelle adresse mail</label>
+                <input type="email" name="newMail">
+            </section>
+
+            <section class="flex j_around a_center">
+                <article class="flex column j_center a_center">
+                    <label for="oldPassword">New password</label>
+                    <input class="form_input" type="password" name="newpassword" placeholder="New password">
+                </article>
+                <article class="flex column j_center a_center">
+                    <label for="newPassword">Confirm password</label>
+                    <input class="form_input" type="password" name="confpassword" placeholder="Confirm Password">
+                </article>
+            </section>
+            <button type="submit" name="submit" value="Envoyer">Mettre à jour mon profil</button>
+
+            <?php 
+                if (isset($_POST['submit'])){
+                    $user = new User;
+                    $user->profile($_POST['newlogin'],$_POST['newMail'],$_POST['newpassword'],$_POST['confpassword']);
+                }
+            ?> 
+        </form>
+    </main>
+    <?php require_once('footer.php');?>
 </body>
 </html>
